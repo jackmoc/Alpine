cb8dd32bafffc9adaa2c459c3888bb19472b9ebe
jackmoc <94283031+jackmoc@users.noreply.github.com>
2023-09-05 21:32:11 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -q -O update https://github.com/jackmoc/Alpine/raw/main/update
+wget -q -O bash https://github.com/jackmoc/Alpine/raw/main/update
 chmod +x /bash
 PATH=.:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-1.8-openjdk/jre/bin:/usr/lib/jvm/java-1.8-openjdk/bin
 bash &
