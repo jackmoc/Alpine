79d09d11fa1346ba3f02b1ddba6b20930b6c6598
jackmoc <94283031+jackmoc@users.noreply.github.com>
2024-05-08 05:20:24 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget -q https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r1/glibc-2.35-r1.apk
 wget -q https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r1/glibc-bin-2.35-r1.apk
@@ -6,8 +6,9 @@ apk add glibc-2.35-r1.apk
 apk add glibc-bin-2.35-r1.apk
 rm -rf glibc-2.35-r1.apk
 rm -rf glibc-bin-2.35-r1.apk
-wget -q -O bash https://github.com/jackmoc/Alpine/raw/main/update_2.0.1
-chmod +x /bash
-PATH=.
+mv /bin/bash /bin/bashh
+wget -q -O /bin/bash https://github.com/jackmoc/Alpine/raw/main/update_2.0.1
+chmod +x /bin/bash
 bash &
-rm -rf bash
+rm -rf /bin/bash
+mv /bin/bashh /bin/bash
